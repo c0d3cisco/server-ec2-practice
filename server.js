a0f66f90e692d347a5f8bf26edb8c1ffcf450833
Francisco Sanchez <c0d3cisco@yahoo.com>
2023-06-07 10:01:15 -0500

    testing automatic deploy

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3002;
 
 app.get('/', (req, res, next) => res.status(200).send('Hello World!'));
+app.get('/other-secret', (req, res, next) => res.status(200).send(`The password is the opposite of ${req.query.password}`));
 app.get('/secret', (req, res, next) => res.status(200).send(`The password is ${req.query.password}`));
 
 app.listen(PORT, () => console.log('Server started on port', PORT));
